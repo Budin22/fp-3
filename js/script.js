c194ceaa15bbdffcf6a9ddc748b648c03b189081
Budin <apserbud22@gmail.com>
2022-03-08 20:49:34 +0100

    done

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+let numberOne = 2;
+let numberTwo = 3;
+let resultPlus = numberOne+numberTwo;
+let resultMinus = numberOne-numberTwo;
+let resultMultiply = numberOne*numberTwo;
+let resultDivide = numberOne/numberTwo;
+
+console.log(resultPlus);
+console.log(resultMinus);
+console.log(resultMultiply);
+console.log(resultDivide);
